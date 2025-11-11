28e8e2afbcc5664e67901ead511f3f78d4f254c9
Sena Çetinkaya <67286519+sena-cetinkaya@users.noreply.github.com>
2025-11-11 15:11:49 +0300

    qa_routes.py

--- app/routes/qa_routes.py
+++ app/routes/qa_routes.py
@@ -11,15 +11,3 @@ class QuestionRequest(BaseModel):
 async def ask_question_endpoint(request: QuestionRequest):
     answer = ask_question(request.question)
     return {"answer": answer}
-
-
-
-"""from fastapi import APIRouter
-from app.services.qa_service import ask_question
-
-router = APIRouter()
-
-@router.post("/ask")
-async def ask_question_endpoint(question: str):
-    answer = ask_question(question)
-    return {"answer": answer}"""
