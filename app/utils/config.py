8a03175dd0202ec7a7b48202d55be76e0b68d2fa
Sena Çetinkaya <67286519+sena-cetinkaya@users.noreply.github.com>
2025-11-11 15:16:54 +0300

    config.py

--- app/utils/config.py
+++ app/utils/config.py
@@ -2,7 +2,8 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-MODEL_PATH = "C:/Users/LENOVO/PycharmProjects/pdf_assistant/mistral-7b-openorca.Q4_0.gguf"
-EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+MODEL_PATH = "your model path"
+EMBEDDING_MODEL = "your embedding model"
 CHROMA_DB_DIR = "vector_db"
 UPLOAD_DIR = "uploaded_pdfs"
+
